ba20437114ca2f7079de6405672cdb1fa7b523ec
Yeken <joaquinastelarra@gmail.com>
2018-08-14 23:38:00 -0300

    added base user model

--- clubApi/controllers/users/authentication.js
+++ clubApi/controllers/users/authentication.js
@@ -1,113 +1,107 @@
-'use strict';
+
 
 const UserManager = require('../../services/user-manager');
 
 class UserController {
-    async login(req, res) {
-        try {
-            let token = await UserManager.login(req.query.email, req.query.password)
-            res.status(200).send({
-                status: 'ok',
-                user: token.user,
-                token: token.token,
-                message: token.message
-            });
-        } catch (e) {
-            throw e;
-            res.status(400).send({
-                status: 'error',
-                error: e
-            });
-
-        }
+  async login(req, res) {
+    try {
+      const token = await UserManager.login(req.param('email'), req.param('password'))
+      res.status(200).send({
+        status: 'ok',
+        user: token.user,
+        token: token.token,
+        message: token.message
+      });
+    } catch (e) {
+      throw e;
+      res.status(400).send({
+        status: 'error',
+        error: e
+      });
     }
-    async changePassword(req, res) {
-        try {
-            var user = await UserManager.getByEmail(req.user.email)
-            if (await UserManager.changePassword(user, req.query))
-                return res.status(200).send({
-                    status: 'ok',
-                    user: user,
-                    message: 'Password Changed Successfully'
-                });
-            else
-                return res.status(400).send({
-                    status: 'error',
-                    error: 'Invalid',
-                    message: 'Invalid Pasword'
-                });
-        } catch (e) {
-            throw e;
-            res.status(400).send({
-                status: 'error',
-                error: e,
-                message: 'Server Error'
-            });
-
-        }
+  }
+  async changePassword(req, res) {
+    try {
+      const user = await UserManager.getByEmail(req.user.email)
+      if (await UserManager.changePassword(user, req.query)) {
+        return res.status(200).send({
+          status: 'ok',
+          user,
+          message: 'Password Changed Successfully'
+        });
+      }
+      return res.status(400).send({
+        status: 'error',
+        error: 'Invalid',
+        message: 'Invalid Pasword'
+      });
+    } catch (e) {
+      throw e;
+      res.status(400).send({
+        status: 'error',
+        error: e,
+        message: 'Server Error'
+      });
     }
-    async retrievePasswordStepOne(req, res) {
-        try {
-            var user = await UserManager.getByEmail(req.query.email)
-            if (await UserManager.resetPasswordStepOne(user))
-                return res.status(200).send({
-                    status: 'ok',
-                    message: 'Retrieve password Link sent Successfully'
-                });
-            else
-                return res.status(400).send({
-                    status: 'error',
-                    error: 'Invalid',
-                    message: 'User not found'
-                });
-        } catch (e) {
-            throw e;
-            res.status(400).send({
-                status: 'error',
-                error: e,
-                message: 'Server Error'
-            });
-
-        }
+  }
+  async retrievePasswordStepOne(req, res) {
+    try {
+      const user = await UserManager.getByEmail(req.query.email)
+      if (await UserManager.resetPasswordStepOne(user)) {
+        return res.status(200).send({
+          status: 'ok',
+          message: 'Retrieve password Link sent Successfully'
+        });
+      }
+      return res.status(400).send({
+        status: 'error',
+        error: 'Invalid',
+        message: 'User not found'
+      });
+    } catch (e) {
+      throw e;
+      res.status(400).send({
+        status: 'error',
+        error: e,
+        message: 'Server Error'
+      });
     }
-    async retrievePasswordStepTwo(req, res) {
-        /* this controller also works for create password for first time
+  }
+  async retrievePasswordStepTwo(req, res) {
+    /* this controller also works for create password for first time
          * when user is created and associated to a customer by ADMIN role
          */
-        try {
-            await UserManager.resetPasswordStepTwo(req, res)
-        } catch (e) {
-            throw e;
-            res.status(400).send({
-                status: 'error',
-                error: e,
-                message: 'Server Error'
-            });
-    
-        }
+    try {
+      await UserManager.resetPasswordStepTwo(req, res)
+    } catch (e) {
+      throw e;
+      res.status(400).send({
+        status: 'error',
+        error: e,
+        message: 'Server Error'
+      });
     }
-    async signUp(req, res) {
-        /*  simple sign up for user */
-        try {
-            var user = await UserManager.signUp(req.query);
-            if (!user._id)
-                return res.status(200).send({status: 'unsuccessfull', error: user.error, message: user.message});
-            delete user._doc.passwordHash;
-            let token = await UserManager.login(req.query.email, req.query.password)
-            res.status(201).send({
-                status: 'created',
-                user: user,
-                token: token.token
-            });
-        } catch (e) {
-            throw e;
-            res.status(400).send({
-                status: 'error',
-                error: e
-            });
-    
-        }
+  }
+  async signUp(req, res) {
+    /*  simple sign up for user */
+    try {
+      const user = await UserManager.signUp(req.query);
+      if (!user._id) { return res.status(200).send({ status: 'unsuccessfull', error: user.error, message: user.message }); }
+      delete user._doc.passwordHash;
+      const token = await UserManager.login(req.query.email, req.query.password)
+      res.status(201).send({
+        status: 'created',
+        user,
+        token: token.token
+      });
+    } catch (e) {
+      throw e;
+      res.status(400).send({
+        status: 'error',
+        error: e
+      });
     }
+  }
 }
 
 module.exports = new UserController();
---
+++ clubApi/models/base-user-model.js
@@ -0,0 +1,46 @@
+
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
+const { USER_ROLES, USER } = require('../constants')
+
+
+const baseUserSchema = new Schema({
+  email: {
+    type: String,
+    validate: {
+      validator(v) {
+        return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(v)
+      },
+      message: '{VALUE} is not a valid email address!'
+    },
+    index: { unique: true },
+    required: true
+  },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  phone: { type: String },
+  active: { type: Boolean, default: true },
+  company_account: { type: Schema.ObjectId, required: false },
+  roles: [{ type: String, enum: USER_ROLES, default: USER }],
+  passwordHash: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+}, { discriminatorKey: 'kind' })
+
+baseUserSchema.pre('save', function (next) {
+  const user = this
+  this.updatedAt = Date.now()
+  if (!user.isModified('passwordHash')) return next()
+  bcrypt.hash(user.passwordHash, 10, function (err, hash) {
+    if (err) {
+      return next(err)
+    }
+    user.passwordHash = hash
+    next()
+  })
+})
+const BaseUserModel = mongoose.model('BaseUserModel', baseUserSchema)
+
+module.exports = BaseUserModel
